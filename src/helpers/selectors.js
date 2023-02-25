0e9641bf607a5322a64a63a71534c0790e352655
bbha99 <branha99@gmail.com>
2023-02-24 21:37:14 -0800

    use api data with useEffect

---
+++ src/helpers/selectors.js
@@ -0,0 +1,28 @@
+export function getAppointmentsForDay(state, day) {
+
+  if (state.days.length === 0) return [];
+
+  const foundDay = state.days.find(d => d.name === day);
+
+  if (!foundDay) return [];
+  
+  return foundDay.appointments.map(appointmentId => state.appointments[appointmentId])
+  // let appointmentsDayList = [];
+  // for (const currDay of state.days) {
+  //   if (currDay.name === day) {
+  //     appointmentsDayList = currDay.appointments;
+  //     break;
+  //   }
+  // }
+  // const keys = Object.keys(state.appointments)
+  // let appointmentsList = [];
+  // for (const appointment of appointmentsDayList) {
+  //   for (const key of keys) {
+  //     if (appointment === Number(key)) {
+  //       appointmentsList.push(state.appointments[key])
+  //     }
+  //   }
+  // }
+
+  // return appointmentsList;
+}
